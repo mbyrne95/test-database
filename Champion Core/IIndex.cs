5ed444facf4f5890ae7ec03a6bb9b85c07f2ec92
mbyrne95 <76924518+mbyrne95@users.noreply.github.com>
2022-06-11 01:28:04 -0400

    more interfaces for db

---
+++ Champion Core/IIndex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ChampionCore
+{
+    public interface IIndex<K, V>
+    {
+        //create new entry at index mapping key K to value V
+        void Insert(K key, V value);
+
+        //find entry by key
+        Tuple<K, V> Get(K key);
+
+        //find all entries that contain key larger than/equal to specified key
+        IEnumerable<Tuple<K, V>> LargerThanOrEqualTo(K key);
+
+        //find all entries larger than key
+        IEnumerable<Tuple<K, V>> LargerThan(K key);
+        
+        //find all entries that contain key less than/equal to specified key
+        IEnumerable<Tuple<K, V>> LessThanOrEqualTo(K key);
+
+        //find all entries less than key
+        IEnumerable<Tuple<K, V>> LessThan(K key);
+
+        //delete entry from this index
+        bool Delete(K key, V value, IComparer<V> valueComparer = null);
+
+        //delete all entries of given key
+        bool Delete(K key);
+    }
+
+}
---
+++ Champion Core/IRecordStorage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChampionCore
+{
+    public interface IRecordStorage
+    {
+        //effectively update a record
+        void Update(uint recordId, byte[] data);
+
+        //grab record data
+        byte[] Find(uint recordId);
+
+        //creates new empty record
+        uint Create();
+
+        //create with given data, return id
+        uint Create(byte[] data);
+
+        //create with other shit
+        uint Create(Func<uint, byte[]> dataGenerator);
+
+        //this deletes a record by its id
+        void Delete(uint recordId);
+    }
+}
