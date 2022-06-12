e5083d1b6eabc70394a5a13e5a2efc98f980cf80
mbyrne95 <76924518+mbyrne95@users.noreply.github.com>
2022-06-12 14:52:35 -0400

    further work on implementation

--- Champion Core/Block.cs
+++ Champion Core/Block.cs
@@ -70,6 +70,83 @@ namespace ChampionCore
 
         public void SetHeader (int field, long value)
         {
+            if (isDisposed)
+            {
+                throw new ObjectDisposedException("Block");
+            }
+            if (field < 0) 
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            //update cache if field is cached
+            if(field < cachedHeaderValue.Length)
+            {
+                cachedHeaderValue[field] = value;
+            }
+
+            //write in cached buffer
+            BufferHelper.WriteBuffer((long)value, firstSector, field * 8);
+            isFirstSectorDirty = true;
+                 
+        }
+
+        public void Read(byte[] dest, int destOffset, int  srcOffset, int count)
+        {
+            if (isDisposed)
+            {
+                throw new ObjectDisposedException("block");
+            }
+
+            //validate argument
+            if (false == ((count >= 0) && ((count + srcOffset) <= storage.BlockContentSize)))
+            {
+                throw new ArgumentOutOfRangeException("requested count is outside of src bounds");
+            }
+
+            if (false == ((count + destOffset) <= dest.Length))
+            {
+                throw new ArgumentOutOfRangeException("requested count is outside of dest bounds");
+            }
+
+            var dataCopied = 0;
+            var copyFromFirstSector = (storage.BlockHeaderSize + srcOffset) < storage.DiskSectorSize;
+
+            if (copyFromFirstSector)
+            {
+                var toBeCopied = Math.Min(storage.DiskSectorSize - storage.BlockHeaderSize - srcOffset, count);
+
+                Buffer.BlockCopy(src: firstSector, srcOffset: storage.BlockHeaderSize + srcOffset, dst: dest, dstOffset: destOffset, count: toBeCopied);
+
+                dataCopied += toBeCopied;
+            }
+
+            if (dataCopied < count)
+            {
+                if (copyFromFirstSector)
+                {
+                    stream.Position = (Id * storage.BlockSize) + storage.DiskSectorSize;
+                } 
+                else
+                {
+                    stream.Position = (Id * storage.BlockSize) + storage.BlockHeaderSize + srcOffset;
+                }
+            }
+
+            while (dataCopied < count)
+            {
+                var bytesToRead = Math.Min(storage.DiskSectorSize, count - dataCopied);
+                var thisRead = stream.Read(dest, destOffset + dataCopied, bytesToRead);
+                if (thisRead == 0)
+                {
+                    throw new EndOfStreamException();
+                }
+                dataCopied += thisRead;
+            }
+        }
+
+        public void Write(byte[] src, int srcOffset, int dstOffset, int count)
+        {
 
         }
     }
